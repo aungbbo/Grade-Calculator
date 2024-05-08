ff2a2d285714ad82c1e1cc591b647089ddb202b8
aungbobo <aungboboyangon02@gmail.com>
2024-05-07 23:33:59 -0700

    updated user input data collection

--- src/main/java/com/example/gradecalculator/HelloController.java
+++ src/main/java/com/example/gradecalculator/HelloController.java
@@ -1,15 +1,15 @@
 package com.example.gradecalculator;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class HelloController implements Initializable {
 
@@ -32,6 +32,18 @@ public class HelloController implements Initializable {
     private ComboBox<String> comboBox3;
 
     @FXML
+    private ComboBox<String> comboBox4;
+
+    @FXML
+    private ComboBox<String> comboBox5;
+
+    @FXML
+    private ComboBox<String> comboBox6;
+
+    @FXML
+    private ComboBox<String> comboBox7;
+
+    @FXML
     private TextField gradeName1;
 
     @FXML
@@ -39,6 +51,18 @@ public class HelloController implements Initializable {
 
     @FXML
     private TextField gradeName3;
+
+    @FXML
+    private TextField gradeName4;
+
+    @FXML
+    private TextField gradeName5;
+
+    @FXML
+    private TextField gradeName6;
+
+    @FXML
+    private TextField gradeName7;
 
     @FXML
     private TextField score1;
@@ -50,10 +74,33 @@ public class HelloController implements Initializable {
     private TextField score3;
 
     @FXML
+    private TextField score4;
+
+    @FXML
+    private TextField score5;
+
+    @FXML
+    private TextField score6;
+
+    @FXML
+    private TextField score7;
+
+    @FXML
     private Label finalGrade;
+
+    @FXML
+    private GridPane gradesGridPane;
+
+    private ComboBox<String>[] comboBoxes;
+    private TextField[] gradeNames;
+    private TextField[] scores;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        gradeNames = new TextField[]{gradeName1, gradeName2, gradeName3, gradeName4, gradeName5, gradeName6, gradeName7};
+        comboBoxes = new ComboBox[]{comboBox1, comboBox2, comboBox3, comboBox4, comboBox5, comboBox6, comboBox7};
+        scores = new TextField[]{score1, score2, score3, score4, score5, score6, score7};
+
 //        comboBox.setItems(FXCollections.observableArrayList("Final", "Project"));
     }
 
@@ -64,9 +111,9 @@ public class HelloController implements Initializable {
                 int weight = Integer.parseInt(categoryWeight.getText());
                 if (weight > 0 && weight <= 100) {
                     if (!comboBox1.getItems().stream().anyMatch(item -> item.contains(categoryName.getText()))) {
-                        comboBox1.getItems().add(String.format("%s (%s%%)", categoryName.getText(), categoryWeight.getText()));
-                        comboBox2.getItems().add(String.format("%s (%s%%)", categoryName.getText(), categoryWeight.getText()));
-                        comboBox3.getItems().add(String.format("%s (%s%%)", categoryName.getText(), categoryWeight.getText()));
+                        for (ComboBox<String> c : comboBoxes) {
+                            c.getItems().add(String.format("%s (%s%%)", categoryName.getText(), categoryWeight.getText()));
+                        }
                         categoryDisplay.setText("");
                         categoryDisplay.setText(String.format("Category \"%s\" is added to the list.", categoryName.getText()));
                     } else {
@@ -86,18 +133,45 @@ public class HelloController implements Initializable {
     @FXML
     void deleteInput(ActionEvent event) {
         if (comboBox1.getItems().stream().anyMatch(item -> item.contains(categoryName.getText()))) {
-            comboBox1.getItems().removeIf(item -> item.contains(categoryName.getText()));
-            comboBox2.getItems().removeIf(item -> item.contains(categoryName.getText()));
-            comboBox3.getItems().removeIf(item -> item.contains(categoryName.getText()));
+            for (ComboBox<String> c : comboBoxes) {
+                c.getItems().removeIf(item -> item.contains(categoryName.getText()));
+            }
             categoryDisplay.setText("");
             categoryDisplay.setText(String.format("Category \"%s\" is removed from the list.", categoryName.getText()));
         }
     }
 
+
+    public int getNumberOfRows() {
+        if (gradesGridPane.getRowConstraints().isEmpty()) {
+            return 0;
+        }
+        return gradesGridPane.getRowConstraints().size();
+    }
+
     @FXML
     void addRow(ActionEvent event) {
+        int rowIndex = getNumberOfRows();
 
+        TextField newGradeName = new TextField();
+        newGradeName.setStyle("-fx-min-height: 31; -fx-max-height: 31px; -fx-min-width: 194px; -fx-max-width: 194px; -fx-margin-left: 2px");
+
+        ComboBox newComboBox = new ComboBox();
+        newComboBox.setStyle("-fx-min-height: 31px; -fx-min-width: 164; -fx-margin: 2px");
+
+        TextField newScore = new TextField();
+        newScore.setStyle("-fx-min-height: 31px; -fx-margin: 2px");
+
+        GridPane.setConstraints(newGradeName, 0, rowIndex + 1);
+        GridPane.setConstraints(newComboBox, 1, rowIndex + 1);
+        GridPane.setConstraints(newScore, 2, rowIndex + 1);
+
+        gradesGridPane.getChildren().addAll(newGradeName, newComboBox, newScore);
+        RowConstraints rowConstraints = new RowConstraints(35);
+//        rowConstraints.setMinHeight(35);
+        gradesGridPane.getRowConstraints().add(rowConstraints);
     }
+
 
     @FXML
     void calculate(ActionEvent event) {
@@ -109,15 +183,9 @@ public class HelloController implements Initializable {
         categoryName.clear();
         categoryWeight.clear();
         categoryDisplay.setText("No Action!");
-        gradeName1.clear();
-        gradeName2.clear();
-        gradeName3.clear();
-        comboBox1.getItems().clear();
-        comboBox2.getItems().clear();
-        comboBox3.getItems().clear();
-        score1.clear();
-        score2.clear();
-        score3.clear();
+        for (TextField gn : gradeNames) { gn.clear(); }
+        for (TextField s : scores) { s.clear(); }
+        for (ComboBox<String> c : comboBoxes) { c.getItems().clear(); }
         finalGrade.setText("");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.gradecalculator {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.gradecalculator to javafx.fxml;
